368c2afa8bb489736de727753ec34c1df2c66b66
daoo <daoo314@gmail.com>
2012-06-06 17:42:46 +0200

    Make default graph more interesting

--- src/Defaults.hs
+++ src/Defaults.hs
@@ -3,23 +3,25 @@ module Defaults where
 import Math.Vector2
 import World
 
-defaultWorld1 :: World
-defaultWorld1 = World
+defaultWorld :: World
+defaultWorld = World
   { worldObjects = 
     [ Object (Vector2 250 250) zero 0
     , Object (Vector2 100 200) zero 1
     , Object (Vector2 200 250) zero 1
     , Object (Vector2 100 540) zero 1
+    , Object (Vector2 500 540) zero 1
     ]
   , worldConnections =
     [ (0, 1)
     , (0, 2)
     , (2, 3)
+    , (3, 4)
     ]
   }
 
-defaultWorld :: World
-defaultWorld = World
+defaultWorld1 :: World
+defaultWorld1 = World
   { worldObjects = 
     [ Object (Vector2 100 250) zero 0
     , Object (Vector2 200 250) zero 1
