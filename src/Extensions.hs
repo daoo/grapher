b8b90ee096fedf6ed9be113e9b0759b0439e5e47
daoo <daoo314@gmail.com>
2012-06-09 20:41:07 +0200

    Documentation plus more functions

--- src/Extensions.hs
+++ src/Extensions.hs
@@ -1,5 +1,6 @@
 module Extensions where
 
+-- |Magic no duplication pairing of all elements in a list
 magic :: ((a, a) -> (a, a)) -> [a] -> [a]
 magic _ []       = []
 magic f (x : xs) = let (x', xs') = g x xs
@@ -9,3 +10,11 @@ magic f (x : xs) = let (x', xs') = g x xs
     g y (z : zs) = let (y', z')   = f (y, z)
                        (y'', zs') = g y' zs
                     in (y'', z' : zs')
+
+foldAll :: (a -> a -> a) -> [a] -> [a]
+foldAll f xs = map (\x -> foldl f x xs) xs
+
+mapIndex :: (a -> a) -> Int -> [a] -> [a]
+mapIndex _ _ []       = []
+mapIndex f 0 (x : xs) = f x : xs
+mapIndex f i (x : xs) = x : mapIndex f (i - 1) xs
