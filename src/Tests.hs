edd12e81549ebd84937e9b8666df1f432d54a424
daoo <daoo314@gmail.com>
2012-06-10 11:59:33 +0200

    Misc clean up

--- src/Tests.hs
+++ src/Tests.hs
@@ -19,10 +19,10 @@ world1 = World [object1, object2] [(0, 1)]
 world2 = World [object1, object2, object3] [(0, 1), (1, 2)]
 
 floatEq :: (Fractional f, Ord f) => f -> f -> Bool
-floatEq a b = (abs $ a - b) < 0.0001
+floatEq a b = abs (a - b) < 0.0001
 
 vector2Eq :: (Fractional f, Ord f) => Vector2 f -> Vector2 f -> Bool
-vector2Eq a b = (magSquared $ a - b) < 0.0001
+vector2Eq a b = magSquared (a - b) < 0.0001
 
 internalForces :: [Object] -> [Vector2 Double]
 internalForces xs = mapCombinations (\a b -> force a b `mult` (normalize $ pos b - pos a)) xs
