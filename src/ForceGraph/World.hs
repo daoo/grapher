5eae8708edf6f61d41d54e374a0470ba756492f2
daoo <daoo314@gmail.com>
2012-08-21 12:10:46 +0200

    Clean up

--- src/ForceGraph/World.hs
+++ src/ForceGraph/World.hs
@@ -29,9 +29,9 @@ showWorld :: World -> String
 showWorld = unlines . map show . worldBalls
 
 iteration :: Time -> World -> World
-iteration t w@(World p r objs) = World p r (map f objs)
+iteration t w = w { worldBalls = map f (worldBalls w) }
   where
-    f = integrate t . limit w . updateBall objs
+    f = integrate t . limit w . updateBall (worldBalls w)
 
 mulVec :: Num a => Vector2 a -> Vector2 a -> Vector2 a
 mulVec (Vector2 ax ay) (Vector2 bx by) = Vector2 (ax * bx) (ay * by)
@@ -56,7 +56,7 @@ render :: Backend a => Settings -> Size -> World -> a ()
 render set (w, h) world = do
   setColor $ getBgColor set
   fillRectangle zero (w, h)
-  setColor $ (0, 0, 0)
+  setColor $ getFgColor set
   strokeCircle (worldLimitRadius world) (worldLimitPos world)
   mapM_ f $ worldBalls world
 
@@ -66,7 +66,3 @@ render set (w, h) world = do
       fillCircle (radius o) (pos o)
       setColor (0.937255, 0.160784, 0.160784)
       fillCircle (radius o * 0.8) (pos o)
-
-    g (o, c) = do
-      setColor $ getFgColor set
-      strokeLine (pos o, pos c)
