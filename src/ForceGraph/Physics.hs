6bdea326b6051b10012a6583c64ffca3f8d164ea
daoo <daoo314@gmail.com>
2012-08-31 10:54:29 +0200

    Add formula for edge drag in the graph

--- src/ForceGraph/Physics.hs
+++ src/ForceGraph/Physics.hs
@@ -5,6 +5,7 @@ import ForceGraph.Ball
 import Math.Algebra
 
 type Force = Vector2D
+type Velocity = Vector2D
 
 repelConst :: Double
 repelConst = 1000.0
@@ -14,3 +15,8 @@ repel :: Ball -> Ball -> Force
 repel a b = f `mult` normalize (pos a - pos b)
   where
     f = (repelConst * charge a * charge b) `divZero` distSquared (pos a) (pos b)
+
+collision :: Ball -> Ball -> Velocity
+collision a b = 1 / (mass a + mass b) `mult` p
+  where
+    p = mass a `mult` vel a + mass b `mult` vel b
