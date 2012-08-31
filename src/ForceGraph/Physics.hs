096601abab74dca2bb69a2bb3dc6869239a2413d
daoo <daoo314@gmail.com>
2012-08-31 09:40:51 +0200

    Clean up

--- src/ForceGraph/Physics.hs
+++ src/ForceGraph/Physics.hs
@@ -4,18 +4,13 @@ import Data.Vector2
 import ForceGraph.Ball
 import Math.Algebra
 
-dragConst, repelConst :: Double
-dragConst = 100.0
+type Force = Vector2D
+
+repelConst :: Double
 repelConst = 10.0
 
--- F = G * m1 * m2 / r^2
-repel :: Ball -> Ball -> Vector2D
-repel x y = f `mult` normalize (pos x - pos y)
+-- F = k * c1 * c2 / r^2
+repel :: Ball -> Ball -> Force
+repel a b = f `mult` normalize (pos a - pos b)
   where
-    f = (repelConst * charge x * charge y) `divZero` distSquared (pos x) (pos y)
-
--- F = r^2 / (D * m1 * m2)
-drag :: Ball -> Ball -> Vector2D
-drag x y = f `mult` normalize (pos y - pos x)
-  where
-    f = distSquared (pos x) (pos y) * invMass x * invMass y / dragConst
+    f = (repelConst * charge a * charge b) `divZero` distSquared (pos a) (pos b)
