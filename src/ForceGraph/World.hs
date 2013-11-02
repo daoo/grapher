c0e2f323160e867a78ee0b41f7baeae018d1e231
daoo <daoo314@gmail.com>
2013-11-02 23:01:04 +0100

    Get rid of sqrts
    Was due to normalization, use some algebra to simplify the expressions.

--- src/ForceGraph/World.hs
+++ src/ForceGraph/World.hs
@@ -77,10 +77,7 @@ attract this other = hookes springConstant (position other) (position this)
 -- |Calculate the spring attraction force from one point to another.
 -- Based on Hooke's law
 hookes :: Float -> Point -> Point -> Force
-hookes k p1 p2 = (k * d) .* n
-  where
-    n = normalize (p1 - p2)
-    d = dist p1 p2
+hookes k p1 p2 = k .* (p1 - p2)
 
 -- |Calculate the force a particle exerts on another particle.
 -- The force (for a positive constant) will be directed from point 2 towards
@@ -91,7 +88,5 @@ interaction :: Float          -- ^ Constant factor
             -> (Point, Float) -- ^ Point 1
             -> (Point, Float) -- ^ Point 2
             -> Force
-interaction c (p1, v1) (p2, v2) = f .* n
-  where
-    f = (c * v1 * v2) `divZero` dist p1 p2
-    n = normalize (p1 - p2)
+interaction c (p1, v1) (p2, v2) = (c * v1 * v2 `divZero` mag2 u) .* u
+  where u = p1 - p2
