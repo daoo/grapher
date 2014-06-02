b9103ed9b1baeb9413e6ee8a23f98d3a9b238365
daoo <daoo314@gmail.com>
2014-06-02 13:53:41 +0200

    Fix benchmark

--- bench/Main.hs
+++ bench/Main.hs
@@ -3,9 +3,11 @@ module Main (main) where
 import Criterion.Main
 import Grapher.Generation
 import Grapher.World
+import Test.QuickCheck.Gen
+import Test.QuickCheck.Random
 
 world :: World
-world = randomWorld 52430753 10000
+world = unGen (arbitraryWorld 10 500) (mkQCGen 532453742) 100
 
 times :: Int -> (a -> a) -> a -> a
 times 0 _ a = a
@@ -13,4 +15,4 @@ times i f a = times (pred i) f (f a)
 
 main :: IO ()
 main = defaultMain
-  [ bench "1000000 iterations" $ whnf (times 1000000 (iteration 0.01)) world ]
+  [ bench "100000 iterations" $ whnf (times 100000 (iteration 0.01)) world ]
