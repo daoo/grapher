bc967fbebe0a79f75904c80dc69b6ca52022f40e
daoo <daoo314@gmail.com>
2015-11-03 20:26:32 +0100

    Fix dot parser

--- src/Grapher/Parser.hs
+++ src/Grapher/Parser.hs
@@ -1,43 +1,61 @@
-{-# LANGUAGE OverloadedStrings, LambdaCase #-}
+{-# LANGUAGE BangPatterns, LambdaCase, OverloadedStrings #-}
 module Grapher.Parser (parseDot) where
 
 import Data.Bifunctor
 import Data.List
 import Data.Maybe
 import Language.Dot.Syntax
+import qualified Data.Map as M
 import qualified Language.Dot.Parser as P
 
 parseDot :: String -> Either String (Int, [(Int, Int)])
 parseDot = bimap show discardGrahpvizInfo . P.parseDot "Grapher.Parser.parseDot"
 
 discardGrahpvizInfo :: Graph -> (Int, [(Int, Int)])
-discardGrahpvizInfo (Graph _ _ _ stmnts) = (length nodes, map (bimap ix ix) edges)
+discardGrahpvizInfo (Graph _ _ _ stmnts) = nodecount $ nub $ sort $ edges stmnts
   where
-    ix :: String -> Int
-    ix = fromJust . (`elemIndex` nodes)
+    nodecount :: [(String, String)] -> (Int, [(Int, Int)])
+    nodecount = bimap fst id . mapAccumL f (0 :: Int, M.empty)
+      where
+        f !acc (a, b) = (acc'', (i, j))
+          where
+            (acc', i)  = g acc a
+            (acc'', j) = g acc' b
 
-    nodes :: [String]
-    nodes = mapMaybe statementnode stmnts
+        g (count, m) a =
+          case M.lookup a m of
+            Nothing -> ((count+1, M.insert a count m), count)
+            Just i  -> ((count, m), i)
 
-    edges :: [(String, String)]
-    edges = concatMap statmentedge stmnts
+    edges :: [Statement] -> [(String, String)]
+    edges = concatMap f
+      where
+        f :: Statement -> [(String, String)]
+        f = \case
+          NodeStatement _ _ -> []
+          EdgeStatement entities _ -> duplicate (pair (mapMaybe edgeid entities))
+          AttributeStatement _ _ -> []
+          AssignmentStatement _ _ -> []
+          SubgraphStatement _ -> []
 
-    statementnode = \case
-      NodeStatement nodeid _ -> nodename nodeid
-      _ -> Nothing
-
-    statmentedge = \case
-      EdgeStatement entities _ -> groupedges (mapMaybe edgeid entities)
-      _ -> []
-
-    nodename (NodeId (NameId str) _)    = Just str
-    nodename (NodeId (StringId str) _)  = Just str
+    nodename :: NodeId -> Maybe String
+    nodename (NodeId (NameId str) _) = Just str
+    nodename (NodeId (StringId str) _) = Just str
     nodename (NodeId (IntegerId int) _) = Just (show int)
-    nodename _                          = Nothing
+    nodename (NodeId (FloatId flt) _) = Just (show flt)
+    nodename (NodeId (XmlId _) _) = Nothing
 
+    edgeid :: Entity -> Maybe String
     edgeid (ENodeId _ nodeid) = nodename nodeid
-    edgeid _                  = Nothing
+    edgeid (ESubgraph _ _) = Nothing
 
-    groupedges []       = []
-    groupedges [_]      = [] -- This is an error really
-    groupedges (a:b:xs) = (a, b) : groupedges xs
+pair :: [a] -> [(a,a)]
+pair []       = []
+pair [_]      = [] -- This is an error
+pair (a:b:xs) = (a, b) : pair (b : xs)
+
+duplicate :: [(a, a)] -> [(a, a)]
+duplicate = concatMap (\x -> [x, swap x])
+  where
+    swap (a, b) = (b, a)
+
